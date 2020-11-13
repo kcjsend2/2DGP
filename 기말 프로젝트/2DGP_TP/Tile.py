98363b6c15ba124cfdfb3deac91e852af9b2c257
kcjsend2 <kcjsend2@naver.com>
2020-11-13 17:33:50 +0900

    json 사용하기 위해 수정

--- 기말 프로젝트/2DGP_TP/Tile.py
+++ 기말 프로젝트/2DGP_TP/Tile.py
@@ -6,10 +6,15 @@ class Tile:
     def __init__(self, x, y, sx, sy, w, h, isCollision):
         self.width = w
         self.height = h
+        self.tpos = [x / 32, y / 32]
         self.pos = [x, y]
         self.image = self.image = gfw.image.load(gobj.RES_DIR + '/PrtWhite.png')
         self.sPos = sx, sy
         self.CollisionMode = isCollision
 
     def draw(self):
-        self.image.clip_draw(*self.sPos, self.width, self.height, *self.pos)
+        self.image.clip_draw_to_origin(*self.sPos, self.width, self.height, *self.pos)
+
+    def dictionary(self):
+        x, y = self.pos
+        return {"x": x, "y": y, "sx": self.sx, "sy": self.sy, "w": self.w, "h": self.h, "isCollision": self.CollisionMode}
--- 기말 프로젝트/2DGP_TP/editor_state.py
+++ 기말 프로젝트/2DGP_TP/editor_state.py
@@ -1,7 +1,9 @@
 import random
 from pico2d import *
 import gfw
+import json
 import gobj
+from Tile import *
 
 canvas_width = 1280
 canvas_height = 960
@@ -15,8 +17,14 @@ VertSelected = 0
 global CollisionMode
 CollisionMode = False
 
+
 def enter():
     gfw.world.init(['platform'])
+    with open('MapData.json', 'r') as fp:
+        data = json.load(fp)
+        for d in data:
+            t = Tile(d.x, d.y, d.sx, d.sy, d.w, d.h, d.isCollision)
+            gfw.world.add('platfrom', t)
 
 
 def update():
@@ -46,13 +54,25 @@ def handle_event(e):
         elif e.key == SDLK_RIGHT and horzSelected < 14:
             horzSelected += 1
         elif e.key == SDLK_UP and VertSelected > 0:
-            horzSelected -= 1
+            VertSelected -= 1
         elif e.key == SDLK_DOWN and VertSelected < 10:
-            horzSelected += 1
+            VertSelected += 1
+        elif e.key == SDLK_s:
+            save_Tile()
+    elif e.type == SDL_MOUSEBUTTONDOWN:
+        if e.key == SDL_BUTTON_LEFT:
+
 
 
 def exit():
     pass
+
+def save_Tile():
+    tile = gfw.world.objects_at(gfw.layer.platform)
+    js_tiles = [t.dictionary() for t in tile]
+
+    with open('MapData.json', 'w') as f:
+        json.dump(js_tiles, f, indent=2)
 
 
 if __name__ == '__main__':
